7500958246817cb8c745c30b696ee0012f9276ad
Brilliant-Atosam <atosam91@gmail.com>
2023-12-21 01:47:23 +0000

    cashlens

--- src/utils/request.js
+++ src/utils/request.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 const request = axios.create({
-  baseURL: "http://localhost:9000/api",
+  baseURL: "https://cashlens-api.netlify.app/api",
 });
 
 export default request;

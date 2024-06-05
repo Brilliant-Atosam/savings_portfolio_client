7ce490b34dfaa44aab17955f57370ed77c486c30
Brilliant-Atosam <atosam91@gmail.com>
2024-06-05 20:26:23 +0100

    highlights

--- src/pages/Notifications.jsx
+++ src/pages/Notifications.jsx
@@ -5,6 +5,7 @@ import useApp from "../useApp";
 import { BsCardHeading } from "react-icons/bs";
 
 const Notifications = () => {
+  console.log("Hi");
   const { user } = useApp();
   const { notifications } = user;
   return (

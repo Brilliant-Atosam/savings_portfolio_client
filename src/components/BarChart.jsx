fc31c5cb29c487d69487039b46c4d2a17838da17
Brilliant-Atosam <atosam91@gmail.com>
2024-06-08 13:15:54 +0100

    highlights design

--- src/components/BarChart.jsx
+++ src/components/BarChart.jsx
@@ -13,7 +13,7 @@ const BarChartComponent = () => {
   return (
     <div className="bar-chart-container">
       <BarChart
-        width={500}
+        width={300}
         height={300}
         data={data}
         margin={{
@@ -28,9 +28,9 @@ const BarChartComponent = () => {
         <YAxis />
         <Tooltip shared={false} trigger="click" />
         <Legend />
-        <Bar dataKey="pv" fill="#8884d8" />
-        <Bar dataKey="uv" fill="#82ca9d" />
-        <Bar dataKey="us" fill="#8cca07" />
+        <Bar dataKey="pv" fill="#8884d8" barSize={15} />
+        <Bar dataKey="uv" fill="#82ca9d" barSize={15} />
+        <Bar dataKey="us" fill="#8cca07" barSize={15} />
       </BarChart>
     </div>
   );
--- src/pages/Highlights.jsx
+++ src/pages/Highlights.jsx
@@ -1,168 +1,141 @@
 import "../styles/highlights.css";
 import Topbar from "../components/Topbar";
 import Util from "../utils/util";
-import useSettings from "../hooks/useSettings";
+// import useSettings from "../hooks/useSettings";
 import BarChartComponent from "../components/BarChart";
 import PieChartComponent from "../components/PieChartComponent";
 import Table from "../components/Table";
 import { IoIosArrowRoundUp } from "react-icons/io";
-import useApp from "../useApp";
+// import useApp from "../useApp";
 import useTableData from "../utils/tableData";
 import useSave from "../hooks/useSave";
 import useExpenses from "../hooks/useExpenses";
 import AreaChartComponent from "../components/AreaChartComponent";
 const Highlights = () => {
   const { format_currency, colors } = Util();
-  const { income_chart_data } = useSettings();
+  // const { income_chart_data } = useSettings();
   const { savingsList, structuredPortfolio } = useSave();
   const { data, expensesList } = useExpenses();
-  const { user } = useApp();
+  // const { user } = useApp();
   const { savingsColumn, expenseColumn } = useTableData();
   return (
     <div className="main-container">
       <Topbar />
+      <h1 className="highlight-heading">Financial Highlights for May 2024</h1>
       <div className="highlight-container">
         <div className="highlight-left">
-          <h1 className="highlight-heading">
-            Financial Highlights for May 2024
-          </h1>
-          <div className="">
-            <h2 className="highlight-subheading">Income for May 2024</h2>
-            <div className="highlights-sub-container finance-info-container">
-              <div className="info-container">
-                <span className="finance-info-key finance-info-key">
-                  Tot. Income
-                </span>
-                <span className="finance-info-value finance-info-value">
-                  {format_currency(2000)}
-                </span>
-              </div>
-              <div className="info-container">
-                <span className="finance-info-key">C2LM</span>
-                <span className="finance-info-value">
-                  2% <IoIosArrowRoundUp />
-                </span>
-              </div>
-              <div className="info-container">
-                <span className="finance-info-key">C2Avg.</span>
-                <span className="finance-info-value">89%</span>
-              </div>
+          <div className="finance-info-container">
+            <h1 className="highlight-title">Income</h1>
+            <h1 className="highlight-subtitle">May, 2024</h1>
+            <div className="info-container">
+              <span className="finance-info-key">Tot. Income</span>
+              <span className="finance-info-value">
+                {format_currency(2000)}
+              </span>
             </div>
-            <BarChartComponent />
-            <div className="piechart-container">
-              <PieChartComponent
-                portfolio={income_chart_data}
-                colors={colors}
-              />
-              <div className="chart-info">
-                {income_chart_data.map((item, index) => (
-                  <div className="ref">
-                    <div
-                      className="indicators"
-                      style={{ background: colors[index] }}
-                    ></div>
-                    <span style={{ color: colors[index], fontSize: "0.8rem" }}>
-                      {item.title}(
-                      {((item.amount / user.total_income) * 100).toFixed(2)}
-                      %)
-                    </span>
-                  </div>
-                ))}
-              </div>
+            <div className="info-container">
+              <span className="finance-info-key">C2LM</span>
+              <span className="finance-info-value">
+                2% <IoIosArrowRoundUp />
+              </span>
             </div>
-            <Table columns={savingsColumn} rows={savingsList} />
+            <div className="info-container">
+              <span className="finance-info-key">C2Av.</span>
+              <span className="finance-info-value">
+                2% <IoIosArrowRoundUp />
+              </span>
+            </div>
           </div>
-          <div className="highlights">
-            <h1 className="highlight-subheading">Savings for May 2024</h1>
-            <div className="finance-info-container">
-              <div className="info-container">
-                <span className="finance-info-key">Tot. Savings</span>
-                <span className="finance-info-value">
-                  {format_currency(2000)}
-                </span>
-              </div>
-              <div className="info-container">
-                <span className="finance-info-key">C2LM</span>
-                <span className="finance-info-value">
-                  2% <IoIosArrowRoundUp />
-                </span>
-              </div>
-              <div className="info-container">
-                <span className="finance-info-key">C2Avg.</span>
-                <span className="finance-info-value">89%</span>
-              </div>
-            </div>{" "}
-            <BarChartComponent />
-            <div className="piechart-container">
+          <div className="highlights-charts-container">
+            <h1 className="highlight-chart-title">Income Charts</h1>
+            <div className="highlights-charts">
+              <BarChartComponent />
               <PieChartComponent
-                portfolio={income_chart_data}
                 colors={colors}
+                portfolio={structuredPortfolio}
               />
-              <div className="chart-info">
-                {income_chart_data.map((item, index) => (
-                  <div className="ref">
-                    <div
-                      className="indicators"
-                      style={{ background: colors[index] }}
-                    ></div>
-                    <span style={{ color: colors[index], fontSize: "0.8rem" }}>
-                      {item.title}(
-                      {((item.amount / user.total_income) * 100).toFixed(2)}
-                      %)
-                    </span>
-                  </div>
-                ))}
-              </div>
-              <div className="savings-portfolios-container">
-                {structuredPortfolio
-                  .filter((item) => !item.archived)
-                  ?.map((item, index) => (
-                    <div className="portfolio" key={index}>
-                      <div className="portfolio-action-container">
-                        <span
-                          style={{ color: `${colors[index]}` }}
-                          className="portfolio-title"
-                        >
-                          {item?.title}({item?.percentage}%)
-                        </span>
-                      </div>
-                      <span
-                        className="portfolio-value"
-                        style={{ color: [colors[index]] }}
-                      >
-                        {format_currency(item?.amount)}
-                      </span>
-                    </div>
-                  ))}
-              </div>
             </div>
+          </div>
+          <div className="finance-info-container">
+            <h1 className="highlight-title">Savings</h1>
+            <h1 className="highlight-subtitle">May, 2024</h1>
+            <div className="info-container">
+              <span className="finance-info-key">Tot. Savings</span>
+              <span className="finance-info-value">
+                {format_currency(2000)}
+              </span>
+            </div>
+            <div className="info-container">
+              <span className="finance-info-key">Net Savings</span>
+              <span className="finance-info-value">
+                {format_currency(2000)}
+              </span>
+            </div>
+            <div className="info-container">
+              <span className="finance-info-key">Efficiency</span>
+              <span className="finance-info-value">2%</span>
+            </div>
+          </div>
+          <div className="highlights-charts-container">
+            <h1 className="highlight-chart-title">Savings Charts</h1>
+            <div className="highlights-charts">
+              <BarChartComponent />
+              <PieChartComponent
+                colors={colors}
+                portfolio={structuredPortfolio}
+              />
+            </div>
+          </div>
+          <div className="highlight-history-container">
+            <h1 className="highlight-chart-title">Income History</h1>
+            <Table columns={savingsColumn} rows={savingsList} />
           </div>
         </div>
         <div className="highlight-right">
-          <div className="highlights">
-            <h2 className="highlight-subheading">Expenses for May 2024</h2>
-            <div className="finance-info-container">
-              <div className="info-container">
-                <span className="finance-info-key">Tot. Expenses</span>
-                <span className="finance-info-value">
-                  {format_currency(2000)}
-                </span>
-              </div>
-              <div className="info-container">
-                <span className="finance-info-key">C2LM</span>
-                <span className="finance-info-value">
-                  2% <IoIosArrowRoundUp />
-                </span>
-              </div>
-              <div className="info-container">
-                <span className="finance-info-key">C2Avg.</span>
-                <span className="finance-info-value">89%</span>
-              </div>
-            </div>{" "}
-            <BarChartComponent />
-            <div className="bar-chart-container">
+          <div className="finance-info-container">
+            <h1 className="highlight-title">Expenses</h1>
+            <h1 className="highlight-subtitle">May, 2024</h1>
+            <div className="info-container">
+              <span className="finance-info-key">Tot. Spendable</span>
+              <span className="finance-info-value">
+                {format_currency(2000)}
+              </span>
+            </div>
+            <div className="info-container">
+              <span className="finance-info-key">Tot. Expenses</span>
+              <span className="finance-info-value">
+                {format_currency(2000)}
+              </span>
+            </div>
+            <div className="info-container">
+              <span className="finance-info-key">SUP</span>
+              <span className="finance-info-value">2%</span>
+            </div>
+          </div>
+          <div className="highlights-charts-container">
+            <h1 className="highlight-chart-title">
+              Spendable & Expenses Charts
+            </h1>
+            <div className="highlights-charts">
+              <BarChartComponent />
+              <PieChartComponent
+                colors={colors}
+                portfolio={structuredPortfolio}
+              />
+            </div>
+          </div>
+          <div className="highlight-history-container">
+            <h1 className="highlight-chart-title">
+              Expense Breakdown by Category
+            </h1>
+            <div className="chart-container">
               <AreaChartComponent data={data} />
             </div>
+          </div>
+          <div className="highlight-history-container">
+            <h1 className="highlight-chart-title">
+              Expense Breakdown by Category
+            </h1>
             <Table columns={expenseColumn} rows={expensesList} />
           </div>
         </div>
